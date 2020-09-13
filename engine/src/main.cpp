d1a5fd92db09bf28b868ff8b571bda1cc90d8ed2
azarem-projects <azarem.projects@gmail.com>
2020-09-13 15:54:11 +0200

    Changed the structure a bit

--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <assimp/Importer.hpp>
-
 #include <core/core.h>
 
 int main()
 {
-    glewExperimental = GL_TRUE;
-    glewInit();
-    glfwInit();
-    Assimp::Importer* importer = new Assimp::Importer();
-
     api::core::initEverything();
+
+    std::cout << "Hello, world!\n";
 
     return 0;
 }
